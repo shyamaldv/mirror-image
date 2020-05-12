439c7155c2c01155c6d240b23a9a93c846151c9f
shyamaldv <shyamal8888@gmail.com>
2020-05-12 14:25:42 +0530

    Create mirror_image.py

---
+++ mirror_image.py
@@ -0,0 +1,52 @@
+"""
+This program takes an image and generates a reflection.
+The top half of the generated image is the same as the original.
+The bottom half is the mirror reflection of the top half.
+"""
+
+from simpleimage import SimpleImage
+
+DEFAULT_FILE = 'images/mt-rainier.jpg'
+
+def make_reflected(filename):
+    image = SimpleImage(filename)
+    width = image.width
+    height = image.height
+
+    # Create new image to contain mirror reflection
+    mirror = SimpleImage.blank(width, height * 2)
+
+    for y in range(height):
+        for x in range(width):
+            pixel = image.get_pixel(x,y)
+            mirror.set_pixel(x,y,pixel)
+            mirror.set_pixel(x, (height * 2) - (y + 1), pixel)
+
+
+
+    return mirror
+
+
+def main():
+    # Get file and load image
+    filename = get_file()
+
+    # Show the original image
+    original = SimpleImage(filename)
+    original.show()
+
+    # Show the reflected image
+    reflected = make_reflected(filename)
+    reflected.show()
+
+
+def get_file():
+    # Read image file path from user, or use the default file
+    filename = input('Enter image file (or press enter for default): ')
+    if filename == '':
+        filename = DEFAULT_FILE
+    return filename
+
+
+if __name__ == '__main__':
+    main()
